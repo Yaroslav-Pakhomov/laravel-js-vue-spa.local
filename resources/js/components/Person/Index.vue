507b127082baff0f7ab40d7ba15ec7358b54069c
Yaroslav-Pakhomov <pahomstyle90@mail.ru>
2022-05-13 11:42:42 +0500

    CRUD in the SPA application, getting a list of persons

--- resources/js/components/Person/Index.vue
+++ resources/js/components/Person/Index.vue
@@ -1,12 +1,47 @@
 <template>
     <div>
         Person Index
+        <table class="table">
+            <thead>
+            <tr>
+                <th scope="col">Имя</th>
+                <th scope="col">Возраст</th>
+                <th scope="col">Профессия</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="person in people">
+                <td>{{ person.name }}</td>
+                <td>{{ person.age }}</td>
+                <td>{{ person.job }}</td>
+            </tr>
+            </tbody>
+        </table>
     </div>
 </template>
 
 <script>
 export default {
-    name: "Index"
+    name: "Index",
+
+    data() {
+        return {
+            people: [],
+        }
+    },
+
+    mounted() {
+        this.getPeople()
+    },
+
+    methods: {
+        getPeople() {
+            axios.get('/api/people')
+                .then(res => {
+                    this.people = res.data
+                })
+        }
+    }
 }
 </script>
 
